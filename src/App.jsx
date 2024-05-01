4ba06929691d28464fe5816a0bd0e20fc4470183
ChristianJoyGaray <cj.garayhello@yahoo.com>
2024-05-01 17:34:55 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import GroceryComponent from "./assets/components/GroceryComponent";
+import "./style.css";
+
+function App() {
+
+  return (
+    <div className="App">
+      <GroceryComponent/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/components/GroceryItemComponent.jsx
@@ -0,0 +1,43 @@
+import React, { useCallback, useState } from 'react'
+
+const GroceryItemComponent = ({item, handleEditItem, handleDeleteItem}) => {
+    const [isEditing, setIsEditing] = useState(false);
+    const [newItem, setNewItem] = useState(item.name);
+    const [errors, setErrors] = useState("");
+
+    const onEdit = () => { 
+
+        if(newItem){
+            handleEditItem(item.id, newItem);
+            setIsEditing(false);
+            setErrors("");
+        }else{
+            setErrors("Grocery Item must not be empty.")
+        }
+    
+    };
+
+  return (
+    <>
+      <li>
+        {isEditing ? (
+        <input type="text" value={newItem} onChange={(event) => setNewItem(event.target.value)}/>
+        ): (
+        <span>{item.name}</span>
+        )}
+        
+        <div>
+            <button onClick={() => {isEditing ? onEdit() : setIsEditing(true);
+            }} 
+            className='btn-edit'>{isEditing ? "Save" : "Edit"}
+            </button>
+            <button onClick={() => handleDeleteItem(item.id)}
+            className='btn-delete'>Delete</button>
+        </div>
+      </li>
+      {errors? <p className='errors'>{errors}</p> : null}
+    </>
+  )
+}
+
+export default GroceryItemComponent
